422c561c12d8889fdc433fe064e488931701a076
haeussma <83341109+haeussma@users.noreply.github.com>
2025-09-25 18:26:48 +0200

    Refactor assertion statements in utility.py for improved readability
    - Simplified assertion formatting in the _resolve_chromatogram function to enhance clarity and maintain consistency in error messages when handling multiple chromatograms.

--- chromhandler/utility.py
+++ chromhandler/utility.py
@@ -19,14 +19,14 @@ def _resolve_chromatogram(
         return chromatograms[0]
 
     if len(chromatograms) > 1:
-        assert wavelength is not None, (
-            "Multiple chromatograms found, but no wavelength is specified."
-        )
+        assert (
+            wavelength is not None
+        ), "Multiple chromatograms found, but no wavelength is specified."
 
         # check that any of the chromatograms has the specified wavelength
-        assert any([chrom.wavelength == wavelength for chrom in chromatograms]), (
-            f"No chromatogram found with wavelength {wavelength} nm."
-        )
+        assert any(
+            [chrom.wavelength == wavelength for chrom in chromatograms]
+        ), f"No chromatogram found with wavelength {wavelength} nm."
 
         return next(chrom for chrom in chromatograms if chrom.wavelength == wavelength)
 
